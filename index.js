e437a1e94aa316a5c9a550b08d8e4df1d61ddfb9
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-09 14:14:51 +0100

    ✨feat(stripe): ajout de la fonction stripe

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
+const stripeRoute = require('./routes/stripe');
 const orderRoute = require('./routes/order');
+
+const cors = require('cors');
 
 dotenv.config();
 
@@ -15,12 +18,15 @@ mongoose.connect(process.env.MONGO_URL)
   .catch((err)=>{console.log(err)
   });
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
 app.use('/api/cart', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
