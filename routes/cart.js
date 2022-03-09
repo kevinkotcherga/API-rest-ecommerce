0a7007dcf70b3bae89533e15e5081f6f3a33bfbf
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-09 11:11:52 +0100

    ✨feat(cart): ajout de la fonction get et get all products

--- routes/cart.js
+++ routes/cart.js
@@ -48,42 +48,27 @@ router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
 	}
 });
 
-// //GET PRODUCT
+//GET USER CART
 
-// router.get('/find/:id', async (req, res) => {
-// 	try {
-// 		const product = await Product.findById(req.params.id);
-// 		const { password, ...others } = user._doc;
-// 		res.status(200).json(product);
-// 	} catch (err) {
-// 		res.status(500).json(err);
-// 	}
-// });
+router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
+	try {
+		const cart = await Cart.findOne({ userId: req.params.userId });
+		const { password, ...others } = user._doc;
+		res.status(200).json(cart);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 
-// //GET ALL PRODUCTS
+//GET ALL
 
-// router.get('/', async (req, res) => {
-// 	const qNew = req.query.new;
-// 	const qCategory = req.query.category;
-// 	try {
-// 		let products;
-
-// 		if (qNew) {
-// 			products = await Product.find().sort({ createdAt: -1 }).limit(5);
-// 		} else if (qCategory) {
-// 			products = await Product.find({
-// 				categories: {
-// 					$in: [qCategory],
-// 				},
-// 			});
-// 		} else {
-// 			products = await Product.find();
-// 		}
-
-// 		res.status(200).json(products);
-// 	} catch (err) {
-// 		res.status(500).json(err);
-// 	}
-// });
+router.get('/', verifyTokenAndAdmin, async (req,res)=>{
+  try{
+    const carts = await Cart.find()
+    res.status(200).json(carts)
+  }catch(err){
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router;
